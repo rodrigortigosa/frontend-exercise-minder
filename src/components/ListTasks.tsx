a65bd7a14180244dda2b5ddb22afd8435b968f63
Rodrigo Ortigosa <rodrigortigosa@gmail.com>
2024-10-10 00:03:08 -0300

    feat: agrega manejo de estados

--- src/components/ListTasks.tsx
+++ src/components/ListTasks.tsx
@@ -1,17 +1,24 @@
-import { Stack } from "@mui/material";
 import { Task } from "../types/Task";
+import { StateTasks } from "../types/StateTasks";
+import { Box, Stack, Typography } from "@mui/material";
 import { TaskItem } from "./TaskItem";
 
 type ListTasksProps = {
+	title: StateTasks;
 	tasks: Task[];
 };
 
-export const ListTasks: React.FC<ListTasksProps> = ({ tasks }) => {
+export const ListTasks: React.FC<ListTasksProps> = ({ title, tasks }) => {
 	return (
-		<Stack sx={{ gap: "16px" }}>
-			{tasks.map((task) => (
-				<TaskItem key={task.id} task={task} />
-			))}
-		</Stack>
+		<Box>
+			<Typography variant="h6" sx={{ mb: "8px" }}>
+				{title}
+			</Typography>
+			<Stack sx={{ gap: "16px" }}>
+				{tasks.map((task) => (
+					<TaskItem key={task.id} task={task} />
+				))}
+			</Stack>
+		</Box>
 	);
 };
--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,17 +1,45 @@
-import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Task } from "../types/Task";
 import { tasks } from "../mocks/tasks";
+import { Box, Stack, Typography } from "@mui/material";
 import { ListTasks } from "../components/ListTasks";
 
 const tasksMock = tasks;
 
 const Tasks = () => {
+	const [pendingTasks, setPendingTasks] = useState<Task[]>([]);
+	const [finishedTasks, setFinishedTasks] = useState<Task[]>([]);
+
+	useEffect(() => {
+		const filteredPendingTasks: Task[] = [];
+		const filteredFinishedTasks: Task[] = [];
+
+		tasksMock.forEach((task) => {
+			if (task.completed) filteredFinishedTasks.push(task);
+			else filteredPendingTasks.push(task);
+		});
+		setFinishedTasks(filteredFinishedTasks);
+		setPendingTasks(filteredPendingTasks);
+	}, []);
+
 	return (
 		<main>
 			<Box sx={{ p: "10px", gap: "10px" }}>
 				<Typography variant="h3" style={{ paddingBottom: "17px" }}>
 					Lista de tareas
 				</Typography>
-				<ListTasks tasks={tasksMock} />
+				<Stack sx={{ gap: "32px", my: "10px" }}>
+					{pendingTasks.length > 0 ? (
+						<ListTasks title={"Pendientes"} tasks={pendingTasks} />
+					) : (
+						<p>No hay tareas pendientes</p>
+					)}
+					{finishedTasks.length > 0 ? (
+						<ListTasks title="Terminadas" tasks={finishedTasks} />
+					) : (
+						<p>No hay tareas terminadas</p>
+					)}
+				</Stack>
 			</Box>
 		</main>
 	);
